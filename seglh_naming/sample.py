4fce2f82b48b280968d87c82c0a502d43c22ab1e
Rachel Duffin <rachel.g.duffin@gmail.com>
2022-11-03 11:21:23 +0000

    Id2 to allow HD and NA prefix

--- seglh_naming/sample.py
+++ seglh_naming/sample.py
@@ -13,7 +13,7 @@ SALT = 'jdhFeducf2gkFb2jj7hjs345klosboiydbo73u7g390yubfkd'
 # sample_name regular expression
 SAMPLE_REGEX = (
     r'([^_]+)_(\d+)_(\d[^_]+)'  # Library_number_DNA
-    r'(?:_(\d[^_]+))?(?:_([^_]{2}))?(?:_([A-Za-z]))?'  # secondary identifiers
+    r'(?:_((?:[A-Z]{2}|\d)\d[^_]+))?(?:_([^_]{2}))?(?:_([A-Za-z]))?'  # secondary identifiers
     r'(?:_([^_]+))?'  # Human readable panel name
     r'_(Pan[^_\.]*)'  # pan number
     r'(?:_(R[A-Z0-9]{2}))?'  # ODS code
@@ -261,7 +261,7 @@ class Sample(object):
 
     @id2.setter
     def id2(self, value):
-        if value and not re.match(r'^[a-zA-Z0-9-]{5,}$', value):
+        if value and not re.match(r'^(:?HD|NA|\d)[a-zA-Z0-9-]{3,}$', value):
             raise ValueError("Secondary identifier invalid ({})".format(value))
         self._id2 = value
 
